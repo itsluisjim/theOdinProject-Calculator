9ce67b77e41837e8486ede23b5f605776def84dc
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-08-05 21:34:27 -0500

    add functionality

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-function add(a,b){
-    return a + b;
-}
-function subtract(a,b){
-    return a - b;
-}
-function multiply(a,b) {
-    return a * b
-}
-function divide(a,b){
-    return a / b ;
+const numberBtns = document.querySelectorAll('.number');
+const operatorsBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.del')
+const previousDisplay = document.querySelector('.previous-operand');
+const currentDisplay = document.querySelector('.current-operand');
+
+function appendNumber (number) {
+    let symbols = /[\-\+\*\/]/;
+    if(currentDisplay.innerText.length == 0 && symbols.test(number)) return;
+    if(number === '.' && currentDisplay.innerText.includes('.')) return;
+    if(symbols.test(number) && symbols.test(currentDisplay.innerText)) return;
+    currentDisplay.innerText += number;
+    
 }
 
-function operate(opp,num1,num2){
-    switch(opp){
-        case '+':
-            return add(num1,num2);
-        case '-':
-            return subtract(num1,num2);
-        case '*':
-            return multiply(num1,num2);
-        case '/':
-            return divide(num1,num2);
-        default:
-            return "OOPS, Error";
-    }
+function updateDisplay() {
+    previousDisplay.innerText = currentDisplay.innerText;
 }
+
+
+function deleteOne() {
+    currentDisplay.innerText = currentDisplay.innerText.substring(0, currentDisplay.innerText.length-1);
+}
+
+function operate(){
+    let result = eval(previousDisplay.innerText);
+
+    if(result == 'Infinity'){
+        result = 'error';
+    }
+    currentDisplay.innerText = result;
+}
+function clear() {
+    currentDisplay.innerText = '';
+    previousDisplay.innerText = '';
+}
+
+//Add eventListeners to all buttons
+numberBtns.forEach(button => {
+    button.addEventListener('click', (e)=> {
+        appendNumber(e.target.innerText);
+        updateDisplay();  
+    })
+});
+
+operatorsBtns.forEach(button => {
+    button.addEventListener('click', (e)=> {
+        appendNumber(e.target.innerText);
+        updateDisplay();
+    })
+});
+
+clearBtn.addEventListener('click', ()=>{
+    clear();
+    updateDisplay();
+});
+
+deleteBtn.addEventListener('click', ()=> {
+    deleteOne();
+})
+
+equalBtn.addEventListener('click',  ()=>  {
+    if(currentDisplay.innerText.length == 0) {
+        clear();
+        return;
+    }
+    updateDisplay();
+    operate();
+    previousDisplay.innerText += ' =';
+
+});
